24b9c10cf4aaa286c1d9afd5101715c1e2f32e0b
lichengxi <lichengxi@baidu.com>
2016-06-06 02:40:16 +0800

    restructure the project

--- ftouch.js
+++ ftouch.js
@@ -2,161 +2,144 @@
  * Created by heyli on 2015/1/9.
  */
 
-/**
- * basic utilities
- */
+;(function() {
 
-var ftouchUtils = (function(){
+    // bind event function
+    var bindEvent = function(object, event, callback) {
+        object.addEventListener(event, function(ev) {
+            (callback)(ev);
+        }, false);
+    };
 
-    var utils = {};
-    try {
+    // change css
+    var renderCss = function(object, styleList) {
+        // browser type
+        var vendors = ['ms', 'moz', 'webkit', 'o'],
+            cssText = '';
 
-        // cross browser css prefix
-        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for (cssStyle in styleList) {
+            if (object) {
+                for (var key in vendors) {
+                    cssText += '-' + vendors[key] + '-' + cssStyle + ':' + styleList[cssStyle] + ';';
+                }
+                cssText += cssStyle + ':' + styleList[cssStyle] + ':';
+            }
+        }
+        object.style.cssText = cssText;
+    };
 
-        // bind events
-        utils.on = function(object, event, callback) {
-            object.addEventListener(event, function(ev) {
-                (callback)(ev);
-            }, false);
-        };
-
-        // add css styles
-        utils.css = function(object, styleList) {
-            var cssText = '';
-            for (cssStyle in styleList) {
-                if (object) {
-                    for (key in vendors) {
-                        cssText += '-' + vendors[key] + '-' + cssStyle + ':' + styleList[cssStyle] + ';';
-                    }
-                    cssText += cssStyle + ':' + styleList[cssStyle] + ':';
+    // check data type
+    var dataType = ["String", "Function", "Array", "Object"],
+        is = {};
+    for (var i = 0, len = dataType.length; i < len; i++) {
+        (function(k) {
+                is[dataType[k]] = function(object) {
+                    return Object.prototype.toString.call(object) === '[object ' + dataType[k] + ']';
                 }
             }
-            object.style.cssText = cssText;
-        };
-    }
-    catch (e) {
-
+        )(i);
     }
 
-    return utils;
-}());
-
-/**
- * core function
- * @param object window object 
- * @param object utilities object
- *
- */
-var ftouch = (function($win, $) {
-
-    // options
-    // wrapper:      dom       event binding dom
-    // isVertical:   boolean   vertical slide or not
-    // debug         boolean   debug mode or not
+    // default options
     var opt = {
-        wrapper: $win.document.body,
-        isVertical: false,
-        debug: true
+        wrapper: window.document.body,  // event binding obj
+        isVertical: false,            
     };
 
-    var config = {
-        direction: 0,               // direction
-        currentDelta: 0             // current moving delta value
+    // internal-sharing touch parameters
+    var params = {
+        direction: 0,
+        touchEndDistance: 0,  // touchend moving distance
+        currentDelta: 0,
+        startPos: null,   // finger starting position
+        movePos: null,    // finger moving position
+        startPosDelta: 0,   //
+        movePosDelta: 0,    //
+        distance: 0         // distance
     };
 
-    // initialization option
-    var init = function(option) {
-        for (key in option) {
+    function FTouch(option, plugins) {
+        var option = option || {}, // user-defined options
+            plugins = plugins || []; // plugins
+
+        for (var key in option) {
             opt[key] = option[key];
         }
+
+        this.register(plugins);
     };
 
-    // register plugin
-    var register = function(object) {
-        var pluginArr = object.split(',');
-        for (var i = 0; i < pluginArr.length; i++) {
-            if (typeof $win[pluginArr[i]] === 'object') {
-                var obj = $win[pluginArr[i]];
-                obj.opt = opt;
-                obj.config = config;
-                obj.$ = $;
-                eventHandler(obj);
-                obj.init(); 
+
+    FTouch.prototype.register = function(plugins) {
+
+        if (is.String(plugins)) {
+            plugins = plugins.split(',');
+        }
+
+        // register plugins
+        for (var i = 0, len = plugins.length; i < len; i++) {
+            if (is.Function(window[plugins[i]])) {
+                var plugin = window[plugins[i]];
+                plugin.prototype.bindEvent = bindEvent,
+                plugin.prototype.renderCss = renderCss;
+                var instance = new plugin(opt, params);
+                instance.init();
+                this.handleEvents(instance);
             }
         }  
+
+
     };
 
-    // get moving direction
-    var getDirection = function(distance) {
+    // get moveing direction
+    FTouch.prototype.getDirection = function(distance) {
         // isVertical = true, 1 -> to bottom, -1 -> to top
         if (distance > 0) {
-            config.direction = -1;
+            params.direction = -1;
         }
         else if (distance < 0) {
-            config.direction = 1;
+            params.direction = 1;
         }
         else {
-            config.direction = 0;
+            params.direction = 0;
         }
 
-        if (config.direction === -1) {
+        if (params.direction === -1) {
             return false;
         }
         return true;
     };
 
-    // event handler function
-    var eventHandler = function(obj) {
-        // customized variables
-        var custVar = {
-            startPos: null,     // finger start point
-            movePos: null,      // finger moving point (real time)
-            startPosDelta: 0,   // finger starting point delta value    
-            movePosDelta: 0,    // finger moving point delta value
-            distance: 0         // moving distance 
-        };
-        $.on(opt.wrapper, 'touchstart', function(ev) {
-            // TODO: should copy instead of directly assign
-            custVar.startPos = ev.touches[0];
-            obj.ontouchstart && obj.ontouchstart(ev, custVar);
+    FTouch.prototype.handleEvents = function(obj) {
+
+        var _this = this;
+
+        bindEvent(opt.wrapper, 'touchstart', function(ev) {
+            params.startPos = ev.touches[0];
+            obj.ontouchstart && obj.ontouchstart(ev, params);
             opt.ontouchstart && opt.ontouchstart();
         });
 
-        $.on(opt.wrapper, 'touchmove', function(ev) {
-            // TODO: should copy instead of directly assign
-            custVar.movePos = ev.touches[0];
-            // assign pageY or pageX value based on verical or horizontal slide
-            custVar.movePosDelta = (opt.isVertical) ? custVar.movePos.pageY : custVar.movePos.pageX;
-            custVar.startPosDelta = (opt.isVertical) ? custVar.startPos.pageY : custVar.startPos.pageX;
-            custVar.distance = custVar.movePosDelta - custVar.startPosDelta;
+        bindEvent(opt.wrapper, 'touchmove', function(ev) {
+            params.movePos = ev.touches[0];
+            params.movePosDelta = (opt.isVertical) ? params.movePos.pageY : params.movePos.pageX;
+            params.startPosDelta = (opt.isVertical) ? params.startPos.pageY : params.startPos.pageX;
+            params.distance = params.movePosDelta - params.startPosDelta;
+            _this.getDirection(params.distance);
 
-            // get finger moving diraction
-            getDirection(custVar.distance);   
-
-            // current moving delta value
-            config.currentDelta += custVar.distance;
+            params.currentDelta += params.distance;
             
-            // callback
-            obj.ontouchmove && obj.ontouchmove(ev, custVar);
+            obj.ontouchmove && obj.ontouchmove(ev, params);
 
-
-            // reassign finger starting point once calculation is done 
-            // TODO: should copy instead of directly assign
-            custVar.startPos = custVar.movePos;
+            params.startPos = params.movePos;
         });
 
-        $.on(opt.wrapper, 'touchend', function(ev) {
-            obj.ontouchend && obj.ontouchend(ev, custVar);
+        bindEvent(opt.wrapper, 'touchend', function(ev) {
+            obj.ontouchend && obj.ontouchend(ev, params);
             opt.ontouchend && opt.ontouchend();
         });
     };
 
-    var ftouch = {
-        opt: opt,
-        init: init,
-        register: register
-    };
+    window.FTouch = FTouch;
 
-    return ftouch;
-}(window, ftouchUtils));
+})();
